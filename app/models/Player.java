654f266dd18080d052e3f3e16f399cdcb4f48a75
Erdan Muhovic <emuhovic@Lux-emuhovic3.local>
2014-04-26 08:40:09 -0400

    Adding score ability

--- app/models/Player.java
+++ app/models/Player.java
@@ -8,8 +8,17 @@ import play.db.jpa.*;
 @Entity
 public class Player extends Model {
 	public String fullname;
+    public int score;
 
+    
 	public Player(String username) {
 		fullname=username;
 	}
+    
+    public void incrementScore (int score){
+        this.score += score;
+    }
+    
 }
+
+
